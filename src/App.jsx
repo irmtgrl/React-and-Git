40e853fbabaad4a133001e18db2d7777fb105f65
İrem Tuğrul <irmtgrl@gmail.com>
2025-09-05 16:29:55 +0300

    Initialized git and react, added heading and parag.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Heading Created in React!</h1>
+      <p>Had a rough start but it's okay now.</p>
+    </div>
+  )
+}
+
+export default App
