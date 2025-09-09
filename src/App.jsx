dcf609e36d83054d4202e4ed119bada816930f56
İrem Tuğrul <irmtgrl@gmail.com>
2025-09-09 19:51:17 +0300

    Using States

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css'
+import React from 'react'
 
 function App() {
-  return (
-    <div>
-      <h1>Heading Created in React!</h1>
-      <p>Had a rough start but it's okay now.</p>
-    </div>
-  )
+  const [cuteStuff, setCuteStuff] = React.useState([])
+
+  const thingsIFindCute = ["Cats", "Happiness", "Books", "Wine"]
+  const displayThings = cuteStuff.map(thing => thing ? <li key={thing}>{thing}</li> : <p>That was it:(</p>)
+
+  function revealThings() {
+    setCuteStuff(prevThing => [...prevThing, thingsIFindCute[cuteStuff.length]])
+  }
+
+return (
+  <main>
+    <section>
+      <h2>Things I Find Cute</h2>
+      <button onClick={revealThings}>Reveal</button>
+      <ul>
+        {displayThings}
+      </ul>
+    </section>
+  </main>
+)
 }
 
 export default App
